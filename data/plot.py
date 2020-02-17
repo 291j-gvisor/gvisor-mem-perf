8c1ef2a3ca6f3f76e540a2128abf49fad274ef44
qobilidop <qobilidop@gmail.com>
2020-02-17 01:55:01 -0800

    Add local benchmark results and tweak plots

--- data/plot.py
+++ data/plot.py
@@ -8,20 +8,32 @@ import seaborn as sns
 THIS_DIR = Path(__file__).parent
 
 def get_bench_names(machine_dir):
-    # return ['malloc_free', 'malloc_nofree']
-    return ['malloc_notouch_nofree','malloc_notouch_free','malloc_touch_nofree','malloc_touch_free']    
+    bench_names = []
+    for path in (machine_dir / 'native').iterdir():
+        if path.is_file():
+            bench_names.append(path.stem)
+    return bench_names
 
 def make_plot(machine_dir, bench_name):
+    print(machine_dir, bench_name)
     df = pd.DataFrame()
-    for runtime_dir in machine_dir.iterdir():
+    for runtime_dir in sorted(machine_dir.iterdir()):
         if not runtime_dir.is_dir():
             continue
         df_ = pd.read_csv(runtime_dir / f'{bench_name}.csv')
         df_['runtime'] = runtime_dir.name
         df = df.append(df_)
-    df['malloc_size_kb'] = df['malloc_size'] // 1024
-    df['ops_per_sec'] = 1 / df['elapsed_time']
-    sns.catplot(x="malloc_size_kb", y="ops_per_sec", hue="runtime", kind="bar", data=df)
+    if 'malloc_size' in df:
+        x_label = 'malloc_size_kb'
+        df[x_label] = df['malloc_size'] // 1024
+    elif 'mmap_size' in df:
+        x_label = 'mmap_size_kb'
+        df[x_label] = df['mmap_size'] // 1024
+    y_label = 'ops_per_sec'
+    df[y_label] = 1 / df['elapsed_time']
+    sns.catplot(x=x_label, y=y_label, hue="runtime", kind="bar", data=df)
+    plt.title(bench_name)
+    plt.tight_layout()
     plt.savefig(machine_dir / f'{bench_name}.pdf')
 
 for machine_dir in THIS_DIR.iterdir():
