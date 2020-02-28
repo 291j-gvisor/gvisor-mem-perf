9ad01e3b16ae2031d69b5bd175e37cbf913a71b4
vagrant <vagrant@vagrant.vm>
2020-02-28 08:55:46 +0000

    Working timestamp system

---
+++ benchmark/run_timestamp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    iter=1
+else
+    iter=$1
+fi
+
+if [ -z "$2" ]; then
+    memsize=1
+else
+    memsize=$2
+fi
+
+sudo rm -rf /tmp/runs
+sudo docker run --runtime=runsc-debug gvisor-mem-perf bin/mmap_anon_nofree $iter $memsize &>tmp
+cat tmp | grep -A99999 -m1 -e 'START MMAP' | grep -B99999 "END MMAP"
+tail -n 1 tmp
