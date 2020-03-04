0e26d0aecd6286e6ce5d274e1fc9ca3635ec3dee
Reapor-Yurnero <reapor.yurnero@gmail.com>
2020-03-04 00:41:34 -0800

    write warmup testbench and run exp1 with two types of warmup

--- benchmark/mmap_shared_nofree.c
+++ benchmark/mmap_shared_nofree.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
   int *map = NULL;
   struct timespec start, end;
   int fd = open("data", O_RDWR | O_CREAT | O_TRUNC, 0600); 
-  printf("%d\n", mapSize);
+  printf("%ld\n", mapSize);
   lseek(fd, mapSize, SEEK_SET);
   clock_gettime(CLOCK_MONOTONIC, &start);
 printf("///////////////////////////////////////////////////\n");
