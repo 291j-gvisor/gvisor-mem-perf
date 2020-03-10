5b78a61d54968f1d07fcc95da0255af67f3e4138
qobilidop <qobilidop@gmail.com>
2020-03-10 11:58:40 -0700

    Update plots

--- data/plot-exp2.py
+++ data/plot-exp2.py
@@ -8,13 +8,15 @@ import pandas as pd
 import seaborn as sns
 
 THIS_DIR = Path(__file__).absolute().parent
-iterations = 25000
 
-def read_data(data_dir):
+def read_data(data_dir, mmap_mode):
     df = pd.DataFrame()
-    for runtime in ['runc', 'runsc-kvm']:
-        base_dir = data_dir / f'{runtime}({iterations})'
-        data_file = base_dir / f'mmap_anon_nofree.csv'
+    for base_dir in sorted(data_dir.iterdir()):
+        if not base_dir.is_dir():
+            continue
+        runtime, iterations = base_dir.name.split("(")
+        iterations = int(iterations[:-1])
+        data_file = base_dir / f'{mmap_mode}.csv'
         if not data_file.exists():
             continue
         df_ = pd.read_csv(data_file)
@@ -31,7 +33,7 @@ def show_values_on_bars(axs):
             _x = p.get_x() + p.get_width() / 2
             _y = p.get_y() + p.get_height()
             value = '{:.2f}'.format(p.get_height())
-            ax.text(_x, _y, value, ha="center")
+            ax.text(_x, _y, value, ha="center", fontsize=8)
 
     if isinstance(axs, np.ndarray):
         for idx, ax in np.ndenumerate(axs):
@@ -39,7 +41,7 @@ def show_values_on_bars(axs):
     else:
         _show_on_single_plot(axs)
 
-def plot_scaling_size(df, data_dir, label):
+def plot_scaling_size(df, data_dir, mmap_mode, exp_name):
     g = sns.catplot(
         x='mmap_size_kb', y='latency_ms', hue='iterations', col='runtime',
         data=df, kind='bar', ci=None,
@@ -47,15 +49,16 @@ def plot_scaling_size(df, data_dir, label):
     show_values_on_bars(g.axes)
     g._legend.remove()
     g.set_axis_labels('mmap Size (KB)', 'Latency (ms)')
-    g.fig.suptitle(f'mmap (private anonymous) nofree, {label}, {iterations} iterations')
+    g.fig.suptitle(f'{mmap_mode}, {exp_name}')
     g.fig.subplots_adjust(top=.9)
     g.savefig(data_dir / f'mmap_scaling_size.pdf')
 
 # Parse arguments
 parser = ArgumentParser()
 parser.add_argument('data_dir', type=Path)
+parser.add_argument('-m', '--mode', default='mmap_anon_nofree')
 args = parser.parse_args()
 data_dir = args.data_dir
 
-df = read_data(data_dir)
-plot_scaling_size(df, data_dir, data_dir.name)
+df = read_data(data_dir, args.mode)
+plot_scaling_size(df, data_dir, args.mode, data_dir.name)
