2e43f4c1e89d929a883a30181177540b0a6cf2df
Reapor-Yurnero <reapor.yurnero@gmail.com>
2020-03-10 21:16:10 -0700

    upload function time measurement for exp2

--- benchmark/exp2.sh
+++ benchmark/exp2.sh
@@ -3,8 +3,11 @@
 sleep 10
 
 # the selected one
-nice -20 taskset 0x1 python3 run_mmap2.py --runtime=runc --iterations=25000
-nice -20 taskset 0x1 python3 run_mmap2.py --runtime=runsc-kvm --iterations=25000
+#nice -20 taskset 0x1 python3 run_mmap2.py --runtime=runc --iterations=25000
+#nice -20 taskset 0x1 python3 run_mmap2.py --runtime=runsc-kvm --iterations=25000
+#nice -20 taskset 0x1 python3 run_mmap2.py --runtime=runsc-dev --iterations=50000
+
+nice -20 taskset 0x1 python3 run_mmap2_timer.py --runtime=runsc-dev --iterations=50000
 
 # without any warmup
 #nice -20 taskset 0x1 ./run_mmap.py --runtime=runc --iterations=100000
